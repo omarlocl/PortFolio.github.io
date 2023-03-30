aa0898bf37c882f4d3e6b5c06acc877c6647848c
omarlocl <balitan221992@gmail.com>
2023-03-30 12:04:09 -0600

    new item

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+var visible = "false";
+
+var bar = document.getElementById("nav-bar");
+bar.onclick = function() {
+    if(visible == "true"){
+        document.getElementById("menu").className = "active";
+        visible = "false";
+    }
+    else {
+        document.getElementById("menu").className = "";
+        visible = "true";
+
+    }
+}
+
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links span');
+    opciones[0].className = "linea";
+    opciones[1].className = "linea";
+    opciones[2].className = "linea";
+    opciones[3].className = "linea";
+    opciones[4].className = "linea";
+    opciones[link].className = "linea seleccionado";
+
+    var x = document.getElementById("menu");
+    x.className = "";arguments
+    visible = "no";
+
+}
+
 
 function efectoHabilidades(){
     var skills = document.getElementById("#skills");
